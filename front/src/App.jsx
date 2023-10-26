b6992cc4314d694ec5a66ebf667312bbdb5897bd
ValentinaBS <vbsanchez16@gmail.com>
2023-10-26 18:33:06 -0300

    feat: nav & footer implementation

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   return (
     <>
-
+      <NavBar />
+      <Footer />
+      {/* <Routes>
+        <Route index element={<Home />} />
+        <Route exact path='/' />
+        <Route path='/about' />
+        <Route path='/about' />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>*/}
     </>
   )
 }
