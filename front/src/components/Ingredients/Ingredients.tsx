d6e629dd063bbbfe8f151902d8cd71d87a6df813
ValentinaBS <vbsanchez16@gmail.com>
2023-11-14 10:48:22 -0300

    refactor: ingredients quantity and syntax

--- front/src/components/Ingredients/Ingredients.tsx
+++ front/src/components/Ingredients/Ingredients.tsx
@@ -1,58 +1,71 @@
 import { useRef, useState } from 'react';
-import IngredientItem  from './IngredientItem'
+import IngredientItem from './IngredientItem'
 import { MagicMotion } from 'react-magic-motion';
 
 export const Ingredients: React.FC = () => {
     const [ingredients, setIngredients] = useState([
-        { id: crypto.randomUUID(), text: '1l water' },
-        { id: crypto.randomUUID(), text: '3 tomatoes' },
+        { id: crypto.randomUUID(), text: 'water', quantity: '1l' },
+        { id: crypto.randomUUID(), text: 'tomatos', quantity: '3' },
     ]);
 
     const newIngredientInput = useRef<HTMLInputElement>(null);
+    const newQuantityInput = useRef<HTMLInputElement>(null);
+
+    const addIngredient = () => {
+        if (!newIngredientInput.current?.value || !newQuantityInput.current?.value) return;
+
+        setIngredients([
+            ...ingredients,
+            {
+                id: crypto.randomUUID(),
+                text: newIngredientInput.current.value,
+                quantity: newQuantityInput.current.value,
+            },
+        ]);
+
+        newIngredientInput.current.value = '';
+        newQuantityInput.current.value = '';
+    };
 
     return (
         <MagicMotion>
             <div>
                 <ul className='d-flex flex-column gap-3 ps-0 overflow-hidden'>
                     {ingredients.map((ingredient) => (
-                        <IngredientItem 
-                            key={ingredient.id} 
-                            ingredient={ingredient} 
-                            setIngredients={setIngredients} 
+                        <IngredientItem
+                            key={ingredient.id}
+                            ingredient={ingredient}
+                            setIngredients={setIngredients}
                         />
                     ))}
                 </ul>
-                <form
-                    onSubmit={(e) => e.preventDefault()}
-                    className='d-flex flex-column flex-md-row gap-3 mt-4'
-                >
-                    <input
-                        ref={newIngredientInput}
-                        type='text'
-                        placeholder='Add a new ingredient...'
-                        className='form-control'
-                    />
+                <div className='d-flex flex-column flex-md-row gap-3 mt-4'>
+                    <div className='d-flex gap-3 w-100'>
+                        <input
+                            ref={newQuantityInput}
+                            type='text'
+                            placeholder='3 kg...'
+                            className='form-control quantity-input'
+                        />
+
+                        <input
+                            ref={newIngredientInput}
+                            type='text'
+                            placeholder='Add a new ingredient...'
+                            className='form-control'
+                        />
+                    </div>
 
                     <button
-                        type='submit'
+                        type='button'
                         title='Add a new ingredient'
-                        className='btn primary-btn col-12 col-md-4'
-                        onClick={(e) => {
-                            e.preventDefault()
-                            if (!newIngredientInput.current?.value) return;
-                            setIngredients([
-                                ...ingredients,
-                                {
-                                    id: crypto.randomUUID(),
-                                    text: newIngredientInput.current.value,
-                                },
-                            ]);
-                            newIngredientInput.current.value = '';
-                        }}
+                        className='btn primary-btn d-flex align-items-center justify-content-center gap-2 add-ingredient-button'
+                        onClick={addIngredient}
                     >
-                        ＋ Add Ingredient
+                        <span>+</span>
+                        Add Ingredient
                     </button>
-                </form>
+                </div>
             </div>
         </MagicMotion>
     )
