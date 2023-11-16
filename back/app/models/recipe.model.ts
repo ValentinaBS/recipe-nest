94497b1195cc8b8109fbb77d5152d0e3d08cf36c
Ana Laura Zanardi <zanardi921@gmail.com>
2023-11-16 10:59:21 -0300

    SavedRecipe

--- back/app/models/recipe.model.ts
+++ back/app/models/recipe.model.ts
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import { pool } from './db'; // Asegúrate de que la importación sea correcta
 import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 
@@ -32,7 +33,7 @@ export class Recipe {
     this.recipe_active = recipe.recipe_active;
     this.recipe_category_occasion = recipe.recipe_category_occasion;
   }
-
+//Crear una receta 
   static async create(newRecipe: any, result: Function): Promise<void> {
     const connection = await pool.getConnection();
     try {
@@ -70,7 +71,7 @@ export class Recipe {
       connection.release();
     }
   }
- 
+ //Crear un comentario en una receta 
   static async addComment(recipeId: number, comment: string, userId: number, result: Function): Promise<void> {
     const newComment = {
       comment_text: comment,
@@ -93,6 +94,51 @@ export class Recipe {
   }
 }
 
+export class SavedRecipe {
+  receta_id: number;
+  user_id: number;
+  user_recipe_id: number;
+
+constructor(savedRecipe: any) {
+    this.receta_id = savedRecipe.receta_id;
+    this.user_id = savedRecipe.user_id;
+    this.user_recipe_id = savedRecipe.user_recipe_id;
+  }
+
+//Guardar una receta en tu perfil
+static async savedRecipe(userId: number, recipeId: number, result: Function): Promise<void> {
+const connection = await pool.getConnection();
+try {
+  const [rows] = await connection.query("INSERT INTO userrecipe (receta_id, user_id) VALUES (?, ?)", [recipeId, userId]);
+  const queryResult = rows as RowDataPacket[];
+  if (queryResult[0] && queryResult[0].insertId) {
+    const insertId = queryResult[0].insertId;
+    console.log("Receta guardada en el perfil con éxito: ", { id: insertId, receta_id: recipeId, user_id: userId });
+    result(null, { id: insertId, receta_id: recipeId, user_id: userId });
+}
+} catch (err) {
+  console.log("Error al guardar la receta en el perfil", err);
+  result(err, null); 
+} finally {
+  connection.release();
+}
+}
+
+//Encuentra la receta guardada en tu perfil
+static async getSavedRecipes(userId: number, result: Function): Promise<void> {
+  const connection = await pool.getConnection();
+  try {
+    const [rows] = await connection.query("SELECT * FROM userrecipe WHERE user_id = ?", userId);
+    console.log(`Recetas guardadas para el usuario con ID ${userId}:`, rows);
+    result(null, rows);
+  } catch (err) {
+    console.log("Error al obtener las recetas guardadas: ", err);
+    result(err, null);
+  } finally {
+    connection.release();
+  }
+}
+}
 
   /*Recipe.updateById = (id, recipe, result) => {
     sql.query(
