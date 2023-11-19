ae083f3ea94702b301e16875f13042b5c23d15b4
ValentinaBS <vbsanchez16@gmail.com>
2023-11-19 10:16:49 -0300

    refactor: notfound component styles

--- front/src/components/NotFound/NotFound.tsx
+++ front/src/components/NotFound/NotFound.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import { NavLink } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
-import "./NotFound.css";
+import './NotFound.css';
 
 const NotFound: React.FC = () => {
   return (
-    <Container className="text-center" style={{ padding: '10% 6% 10% 6%' }}>
-      <Row className="alin-items-center mb-3">
-        <Col xs={12} md={6} className="mb-2">
-          <Image className="imagen" src="public/Green (1) 4 (1).png" rounded />
+    <Container className='text-center mx-auto' style={{ padding: '10% 0' }}>
+      <Row className='align-items-center mb-3 mx-0'>
+        <Col xs={12} lg={6} className='my-4 my-lg-0'>
+          <Image className='notfound-img' src='/Green (1) 4 (1).png' rounded />
         </Col>
-        <Col xs={12} md={6}>
+        <Col xs={12} lg={6}>
           <Card.Body>
-            <Card.Title className="text-font"> oh no! </Card.Title>
-            <Card.Text>
-              <p>We can't find the page you're looking for, but don't worry!</p>
+            <Card.Title className='notfound-title'> Oh no! </Card.Title>
+            <Card.Text className='my-3 px-3'>
+              <p className='mb-0'>We can't find the page you're looking for, but don't worry!</p>
               <p>You can still go to our recipe catalog and browse them all!</p>
             </Card.Text>
-            <Button variant="secondary">Go to recipes <FaArrowRight /></Button>
+            <NavLink to='/search' className='btn primary-btn mt-3 py-2 px-4'>
+              Go to recipes <FaArrowRight className='ms-2' />
+            </NavLink>
           </Card.Body>
         </Col>
       </Row>
