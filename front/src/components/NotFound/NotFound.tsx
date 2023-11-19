6bc668586f3580e074ee3a0340d0163e624dca54
Belen Delgadillo <belendelgadilloarias@gmail.com>
2023-11-19 02:36:54 -0300

    fixed component

--- front/src/components/NotFound/NotFound.tsx
+++ front/src/components/NotFound/NotFound.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { FaArrowRight } from 'react-icons/fa'; 
-import './NotFound.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import { FaArrowRight } from 'react-icons/fa';
+import "./NotFound.css";
 
 const NotFound: React.FC = () => {
   return (
-    <div className="not-found-container">
-      <img src="public/Green (1) 4 (1).png" alt="Image not found" />
-      <div className="not-found-content">
-        <h3>oh no!</h3>
-        <p>We can't find the page you're looking for, but don't worry!</p>
-        <p>You can still go to our recipe catalog and browse them all!</p>
-        <button className="custom-button">
-          Go to recipes <FaArrowRight /> 
-        </button>
-      </div>
-    </div>
+    <Container className="text-center" style={{ padding: '10% 6% 10% 6%' }}>
+      <Row className="alin-items-center mb-3">
+        <Col xs={12} md={6} className="mb-2">
+          <Image className="imagen" src="public/Green (1) 4 (1).png" rounded />
+        </Col>
+        <Col xs={12} md={6}>
+          <Card.Body>
+            <Card.Title className="text-font"> oh no! </Card.Title>
+            <Card.Text>
+              <p>We can't find the page you're looking for, but don't worry!</p>
+              <p>You can still go to our recipe catalog and browse them all!</p>
+            </Card.Text>
+            <Button variant="secondary">Go to recipes <FaArrowRight /></Button>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Container>
   );
 };
-
 export default NotFound;
