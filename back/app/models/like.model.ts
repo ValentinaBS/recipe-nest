7258940d147981d57c41b131e0fc33569b789d15
Zoe Carida <Zowycar@gmail.com>
2023-11-20 15:15:45 -0300

    refactor:modific-like-model

--- back/app/models/like.model.ts
+++ back/app/models/like.model.ts
@@ -1,6 +1,22 @@
-import { pool } from './db';
+import { pool } from "./db";
 
-export interface Like{
+
+/*export interface Like{
     recipe_id: Number;
+    recipe_like: Number;
+    user_id: Number;}*/
+
+export class Like {
+
+    recipe_id: Number;
+    recipe_like: Number;
     user_id: Number;
-    recipe_like: Number;}
+
+    constructor(like: any){
+        this.recipe_id = like.recipe_id;
+        this.recipe_like = like.recipe_like;
+        this.user_id = like.user_id;
+    }
+    
+
+}
